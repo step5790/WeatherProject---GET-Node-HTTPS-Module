dcd12195b6354c61cd3646b6049d7c0dc4b6ec38
Stephen <stephen.s.espinosa@gmail.com>
2023-07-04 09:50:37 +0200

    Body-parse to parse POST request to server

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 const express = require("express");
 const app = express();
 const https = require("https");
+const bodyParser = require("body-parser");
 
+// used body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// listen to server
 app.listen(3000, function () {
   console.log("server running port 3000");
 });
 
-// what happen when user go to root rouite (home)
-
-app.get("/", function (req, res) {
+// send user input to body-parser
+app.post("/", function (req, res) {
   // using https module to get url
+  const query = req.body.cityName;
+  const apiKey = "3465bd2a83d24c468bbd2cfa676763f6";
+  const url = `https://api.weatherbit.io/v2.0/current?city=${query}&key=${apiKey}`;
+  console.log(url);
 
-  const url =
-    "https://api.weatherbit.io/v2.0/current?city=copenhagen&key=3465bd2a83d24c468bbd2cfa676763f6";
-
+  // get https data
   https.get(url, function (response) {
     // call method to get data from the url
     response.on("data", function (data) {
@@ -27,12 +33,17 @@ app.get("/", function (req, res) {
       const imgURL = "https://cdn.weatherbit.io/static/img/icons";
 
       // send data using res.write **can have multiple write
-      res.write(`<p>The weather is currently: ${descr}</p>`);
-      res.write(`<h1>The temperature is: ${temp} degress Celcius</h1>`);
+      res.write(`<h1>The weather in ${query} is currently: ${descr}</h1>`);
+      res.write(`<h3>The temperature is: ${temp} degress Celcius</h3>`);
       res.write(`<img src="${imgURL}/${icon}.png">`);
 
       // send data using res from  app.get **can only have one response/res in one app.get (method)
       res.send();
     });
   });
+});
+
+// get data and send to index.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
